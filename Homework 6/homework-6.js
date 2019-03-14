9c4be79e5ada13fdddb4b200aac126939f084d50
antonio.hadrovic <hadrovic1@gmail.com>
2019-03-14 15:55:55 +0100

    Add array of halls

---
+++ Homework 6/homework-6.js
@@ -0,0 +1,59 @@
+//halls
+
+var hall01 = {
+    id: 1,
+    name: "hall-name",
+    reservation: {
+        isReserved: false,
+        reservedFrom: null,
+        reservedUntil: null,
+    }
+}
+
+
+var hall02 = {
+  id: 2,
+  name: "hall-name",
+  reservation: {
+    isReserved: false,
+      reservedFrom: null,
+      reservedUntil: null,
+  }
+}
+
+
+var hall03 = {
+  id: 3,
+  name: "hall-name",
+  reservation: {
+      isReserved: false,
+      reservedFrom: null,
+      reservedUntil: null,
+  }
+}
+
+
+var hall04 = {
+  id: 4,
+  name: "hall-name",
+  reservation: {
+    isReserved: false,
+    reservedFrom: null,
+    reservedUntil: null,
+  }
+}
+
+
+var hall05 = {
+  id: 5,
+  name: "hall-name",
+  reservation: {
+      isReserved: false,
+      reservedFrom: null,
+      reservedUntil: null,
+  }
+}
+
+
+//the array
+halls = [hall01, hall02, hall03, hall04, hall05];
